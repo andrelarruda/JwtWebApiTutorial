1d817b0349b800e38101ca1b735d44e38880f6f8
andre <andre@ANDRE-LAPTOP>
2023-02-18 00:35:31 -0300

    Add project files.

---
+++ JwtWebApiTutorial/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace JwtWebApiTutorial
+{
+    // This class will be used in the login. To store the username and password given by the login client.
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    } 
+}
